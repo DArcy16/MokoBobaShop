757ef7d8ec21bdac4ca507366bafaef8f788bf10
DArcy16 <walterluke1612@gmail.com>
2022-11-25 15:14:51 +0630

    bg

--- src/App.jsx
+++ src/App.jsx
@@ -92,7 +92,7 @@ function App() {
 
   return (
     <MenuContext.Provider value={menuContextValue}>
-      <main className='w-full md:w-5/6 h-full bg-neutral mx-auto box-border'>
+      <main className='w-full md:w-5/6 h-full bg-base-300 mx-auto box-border'>
       
 
         
